171eacee34f0bf3640fe3d0678553530000043d2
shahrooz <sh.mjsoft@gmail.com>
2012-07-29 21:43:50 +0430

    Second Commit

--- BreakAwayConsole/Program.cs
+++ BreakAwayConsole/Program.cs
@@ -6,9 +6,13 @@ namespace BreakAwayConsole
 	internal class Program
 	{
 		private static void Main(string[] args)
-		{App_Start.EntityFrameworkProfilerBootstrapper.PreStart();
+		{
+			App_Start.EntityFrameworkProfilerBootstrapper.PreStart();
 
 			Database.SetInitializer(new InitializeBagaDatabaseWithSeedData());
+
+			Query.Query.PrintAllDestination();
+			System.Console.ReadLine();
 		}
 	}
 }
---
+++ BreakAwayConsole/Query/Query.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataAccess;
+
+namespace BreakAwayConsole.Query
+{
+	public class Query
+	{
+		public static void PrintAllDestination()
+		{
+			using (var context = new BreakAwayContext())
+			{
+				foreach (var destination in context.Destinations)
+				{
+					Console.WriteLine(destination.Name);
+				}
+			}
+		}
+	}
+}
